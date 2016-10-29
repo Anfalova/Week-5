2f1930f65127f8d144b5e64931ab67817119a262
Евгений Анфалов <anfalovd10@yandex.ru>
2016-10-29 22:38:54 +0500

    fix

---
+++ Processor/Processor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Processor
+{
+    public class Processor<T, TRequest>
+    {
+        protected Func<TRequest, bool> Check;
+        protected Func<TRequest, T> Register;
+        protected Action<T> Save;
+
+        public Processor(Func<TRequest, bool> Check, Func<TRequest, T> Register, Action<T> Save)
+        {
+            this.Check = Check;
+            this.Register = Register;
+            this.Save = Save;
+        }
+
+        public T Process(TRequest request)
+        {
+            if (!Check(request))
+                throw new ArgumentException();
+            var result = Register(request);
+            Save(result);
+            return result;
+        }
+    }
+}
---
+++ TransactionProcessor/TransactionProcessor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransactionProcessor
+{
+    public class Transaction
+    {
+    }
+
+    public class TransactionRequest
+    {
+    }
+
+    public class TransactionProcessor
+    {
+        protected Func<TransactionRequest, bool> Check;
+        protected Func<TransactionRequest, Transaction> Register;
+        protected Action<Transaction> Save;
+
+        public TransactionProcessor(Func<TransactionRequest, bool> Check,
+            Func<TransactionRequest, Transaction> Register, Action<Transaction> Save)
+        {
+            this.Check = Check;
+            this.Register = Register;
+            this.Save = Save;
+        }
+
+        public Transaction Process(TransactionRequest request)
+        {
+            if (!Check(request))
+                throw new ArgumentException();
+            var result = Register(request);
+            Save(result);
+            return result;
+        }
+    }
+}
